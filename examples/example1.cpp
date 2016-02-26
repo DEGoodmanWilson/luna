2a4ef2f263cae2c032aa66b87f5a692c4222379a
Don Goodman-Wilson <don@slack-corp.com>
2016-02-26 12:00:29 -0800

    A new example to mirror the docs

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -4,37 +4,17 @@
 
 using namespace luna;
 
+response hello_world(const endpoint_matches &matches,
+                     const query_params     &params)
+{
+    return {"<h1>Hello, World!</h1>"};
+}
+
 int main(void)
 {
-    server server{server::mime_type{"text/json"}, server::port{8443},
-            //custom logger
-                  [](const std::string mesg)
-                      {
-                          std::cout << mesg << std::endl;
-                      }
-    };
+    server server{server::port{8443}};
 
-    server.handle_request(request_method::GET,
-                          "/ohyeah",
-                          [](auto matches, auto params) -> response
-                              {
-                                  return {"{\"koolade\": true}"};
-                              });
+    server.handle_request(request_method::GET, "/hello_world", &hello_world);
 
-    server.handle_request(luna::request_method::POST,
-                          "/test",
-                          [](auto matches, auto params) -> response
-                              {
-                                  return {"hello"};
-                              });
-
-    server.handle_request(request_method::GET,
-                          "^/documents/(i[0-9a-f]{6})",
-                          [](auto matches, auto params) -> response
-                              {
-                                  auto document_id = matches[1];
-                                  return {"text/html", "<h1>Serving up document " + document_id + "</h1>"};
-                              });
-
-    while (server); //idle while the server is running. Maybe not the best way? //TODO how to signal to server to die
+    while (server); //run forever, basically, or until the server decides to kill itself.
 }
