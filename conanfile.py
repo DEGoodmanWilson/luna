3697eb8a1491a11bc806d896533545c20fa44934
Don Goodman-Wilson <don@slack-corp.com>
2016-03-07 17:33:57 -0800

    Progress

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, CMake
+
+class LunaConan(ConanFile):
+    name = "luna"
+    version = "1.0.0-beta5"
+    url = "https://github.com/DEGoodmanWilson/luna.git"
+    license = "Apache 2.0"
+    requires = "cpr/1.2.0@cpr/testing"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"build_shared_libs":   [True, False],
+               "build_luna_tests":    [True, False],
+               "build_luna_coverage": [True, False],
+               "build_luna_examples": [True, False]}
+    default_options = "build_shared_libs=False", "build_luna_tests=False", "build_luna_coverage=False", "build_luna_examples=False"
+
+    def source(self):
+        self.run("git clone https://github.com/DEGoodmanWilson/luna.git --branch v%s" % (self.version))
+
+    def config(self):
+        if self.options.build_luna_tests:
+            self.requires.add("gtest/1.7.0@lasote/stable", private=False)
+            self.options["gtest"].shared = False
+        else:
+            if "gtest" in self.requires:
+                del self.requires["gtest"]
+
+    def build(self):
+        cmake = CMake(self.settings)
+        build_shared_libs = "-DBUILD_SHARED_LIBS=ON" if self.options.build_shared_libs else ""
+        build_luna_tests = "-DBUILD_LUNA_TESTS=ON" if self.options.build_luna_tests else ""
+        build_luna_coverage = "-DBUILD_LUNA_COVERAGE=ON" if self.options.build_luna_coverage else ""
+        build_luna_examples = "-DBUILD_LUNA_EXAMPLES=ON" if self.options.build_luna_examples else ""
+
+        self.run('cmake %s %s %s %s "%s/luna" %s' % (build_shared_libs, build_luna_tests, build_luna_coverage, build_luna_examples, self.conanfile_directory, cmake.command_line))
+        self.run('cmake --build . %s' % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include/luna", src="luna")
+        self.copy("*.lib", dst="lib", src="lib")
+        self.copy("*.a", dst="lib", src="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["luna"]
