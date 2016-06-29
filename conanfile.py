df7a7ace1182b64a03ef19c41de7ca345d5aca3a
Don Goodman-Wilson <don@slack-corp.com>
2016-06-29 08:24:56 -0700

    Maybe this will work.

--- conanfile.py
+++ conanfile.py
@@ -34,7 +34,7 @@ class LunaConan(ConanFile):
         build_luna_coverage = "-DBUILD_LUNA_COVERAGE=ON" if self.options.build_luna_coverage else ""
         build_luna_examples = "-DBUILD_LUNA_EXAMPLES=OFF" if not self.options.build_luna_examples else ""
 
-        self.run('cmake %s %s %s %s "%s/luna" %s' % (build_shared_libs, build_luna_tests, build_luna_coverage, build_luna_examples, self.conanfile_directory, cmake.command_line))
+        self.run('cmake %s %s %s %s "%s" %s' % (build_shared_libs, build_luna_tests, build_luna_coverage, build_luna_examples, self.conanfile_directory, cmake.command_line))
         self.run('cmake --build . %s' % cmake.build_config)
 
     def package(self):
