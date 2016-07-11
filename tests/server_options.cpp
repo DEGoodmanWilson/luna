2b395f7f8f79d0ed53d6f3e5f476d07a47a12db8
Don Goodman-Wilson <don@slack-corp.com>
2016-07-10 20:15:06 -0700

    More tests on callbacks

--- tests/server_options.cpp
+++ tests/server_options.cpp
@@ -12,8 +12,7 @@
 
 TEST(server_options, set_mime_type)
 {
-    luna::server server{luna::server::port{8080},
-                        luna::server::mime_type{"howdyho"}
+    luna::server server{luna::server::mime_type{"howdyho"}
     };
     server.handle_request(luna::request_method::GET,
                           "/test",
@@ -29,5 +28,62 @@ TEST(server_options, set_mime_type)
     ASSERT_EQ("howdyho", res.header["Content-Type"]);
 }
 
-//This is going to be tough, as for the moment only indirect tests are possible. Would be nice to be able to sub
-// in our own impl to make this work.
+TEST(server_options, set_error_handler_cb)
+{
+    luna::server server{luna::server::error_handler_cb
+                                {
+                                        [](luna::response &response, //a hook for modifying in place to insert default content
+                                           luna::request_method method,
+                                           const std::string &path)
+                                            {
+                                                ASSERT_EQ(404, response.status_code);
+                                                ASSERT_EQ("/test", path);
+                                                ASSERT_EQ(luna::request_method::GET, method);
+                                            }
+                                }
+    };
+
+    auto res = cpr::Get(cpr::Url{"http://localhost:8080/test"}, cpr::Parameters{{"key", "value"}});
+}
+
+TEST(server_options, set_accept_policy_cb)
+{
+    luna::server server{luna::server::accept_policy_cb
+        {
+            [](const struct sockaddr *add, socklen_t len) -> bool
+            {
+                //we can't really know what address will get passed in so just assert that
+                // returning "false" does the right thing, and that we got called.
+                EXPECT_NE(nullptr, add);
+                return false;
+            }
+        }
+    };
+
+    auto res = cpr::Get(cpr::Url{"http://localhost:8080/test"}, cpr::Parameters{{"key", "value"}});
+    ASSERT_EQ(cpr::ErrorCode::EMPTY_RESPONSE, res.error.code);
+}
+
+TEST(server_options, set_unescaper_cb)
+{
+    luna::server server{luna::server::unescaper_cb
+        {
+            [](const std::string &text) -> std::string
+            {
+                    if(text == "value") return "ugh";
+                    return text;
+            }
+        }
+    };
+
+    server.handle_request(luna::request_method::GET,
+                          "/test",
+                          [](auto matches, auto params) -> luna::response
+                          {
+                              EXPECT_EQ("ugh", params["key"]);
+                              return {params["key"]};
+                          });
+
+    auto res = cpr::Get(cpr::Url{"http://localhost:8080/test"}, cpr::Parameters{{"key", "value"}});
+    ASSERT_EQ("ugh", res.text);
+}
