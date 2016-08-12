1f7399b1b69c311efb9dadf73241c590102da87a
Don Goodman-Wilson <don@slack-corp.com>
2016-08-13 08:48:41 +1000

    Bump version to 2.0

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class LunaConan(ConanFile):
     name = "luna"
-    version = "1.1.0"
+    version = "2.0"
     url = "https://github.com/DEGoodmanWilson/luna.git"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
