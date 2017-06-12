c9631ffee016773a267c52d86495ac2daed2e87a
Don Goodman-Wilson <don@slack-corp.com>
2017-06-12 12:08:24 +0200

    Getting tests in order

--- tests/options.cpp
+++ tests/options.cpp
@@ -10,23 +10,23 @@
 #include <cpr/cpr.h>
 
 // At the moment these crash. Why?
-//TEST(options, default_404)
-//{
-//    luna::server server{luna::server::port{8080}};
-//    auto res = cpr::Options(cpr::Url{"http://localhost:8080/"});
-//    ASSERT_EQ(404, res.status_code);
-//}
-//
-//TEST(options, default_200)
-//{
-//    luna::server server{luna::server::port{8080}};
-//    server.handle_request(luna::request_method::OPTIONS,
-//                          "/test",
-//                          [](auto req) -> luna::response
-//                          {
-//                              return {"hello"};
-//                          });
-//    auto res = cpr::Options(cpr::Url{"http://localhost:8080/test"}, cpr::Parameters{});
-//    ASSERT_EQ(200, res.status_code);
-//    ASSERT_EQ("hello", res.text);
-//}
+TEST(options, default_404)
+{
+    luna::server server{luna::server::port{8080}};
+    auto res = cpr::Options(cpr::Url{"http://localhost:8080/"});
+    ASSERT_EQ(404, res.status_code);
+}
+
+TEST(options, default_200)
+{
+    luna::server server{luna::server::port{8080}};
+    server.handle_request(luna::request_method::OPTIONS,
+                          "/test",
+                          [](auto req) -> luna::response
+                          {
+                              return {"hello"};
+                          });
+    auto res = cpr::Options(cpr::Url{"http://localhost:8080/test"}, cpr::Parameters{});
+    ASSERT_EQ(200, res.status_code);
+    ASSERT_EQ("hello", res.text);
+}
