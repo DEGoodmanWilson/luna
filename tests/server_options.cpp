984c46f4bd877aaf3ccd9d8364ac6656fa87ae3c
Don Goodman-Wilson <don@slack-corp.com>
2017-06-13 15:22:16 +0200

    Fix the threading option tests so they aren't indeterministic as much.

--- tests/server_options.cpp
+++ tests/server_options.cpp
@@ -104,25 +104,21 @@ TEST(server_options, set_unescaper_cb)
 
 TEST(server_options, set_thread_pool_size)
 {
-    uint8_t count = 0;
-    uint8_t max_count = 0;
+    std::map<std::thread::id, uint16_t> thread_counter;
     std::mutex mutex;
+    const int thread_pool_size{5};
 
-    luna::server server{luna::server::thread_pool_size{5}};
+    luna::server server{luna::server::thread_pool_size{thread_pool_size}};
     server.handle_request(luna::request_method::GET,
                           "/test",
-                          [&count, &max_count, &mutex](auto req) -> luna::response
+                          [&thread_counter, &mutex](auto req) -> luna::response
                               {
                                   mutex.lock();
-                                  ++count;
-                                  if (count > max_count) max_count = count;
+                                  thread_counter[std::this_thread::get_id()] += 1;
                                   mutex.unlock();
 
                                   std::this_thread::sleep_for(100ms);
 
-                                  mutex.lock();
-                                  --count;
-                                  mutex.unlock();
                                   return {"Hello"};
                               });
 
@@ -141,50 +137,45 @@ TEST(server_options, set_thread_pool_size)
         t.join();
     }
 
-    ASSERT_EQ(0, count);
-    ASSERT_EQ(5, max_count);
+//    ASSERT_EQ(0, count);
+    ASSERT_EQ(thread_pool_size, thread_counter.size());
 }
 
 TEST(server_options, use_thread_per_connection)
 {
-    uint8_t count = 0;
-    uint8_t max_count = 0;
+    std::map<std::thread::id, uint16_t> thread_counter;
     std::mutex mutex;
 
     luna::server server{luna::server::use_thread_per_connection{true}};
     server.handle_request(luna::request_method::GET,
                           "/test",
-                          [&count, &max_count, &mutex](auto req) -> luna::response
-                              {
-                                  mutex.lock();
-                                  ++count;
-                                  if (count > max_count) max_count = count;
-                                  mutex.unlock();
+                          [&thread_counter, &mutex](auto req) -> luna::response
+                          {
+                              mutex.lock();
+                              thread_counter[std::this_thread::get_id()] += 1;
+                              mutex.unlock();
 
-                                  std::this_thread::sleep_for(10ms);
+                              std::this_thread::sleep_for(100ms);
 
-                                  mutex.lock();
-                                  --count;
-                                  mutex.unlock();
-                                  return {"Hello"};
+                              return {"Hello"};
                               });
 
-    std::thread threads[10];
-    for (int x = 0; x < 10; ++x)
+    const int thread_count{50};
+    std::array<std::thread, thread_count> threads;
+    for (auto &t : threads)
     {
-        threads[x] = std::thread{[]()
-                                     {
-                                         cpr::Get(cpr::Url{"http://localhost:8080/test"});
-                                     }};
+        t = std::thread{[]()
+                        {
+                            cpr::Get(cpr::Url{"http://localhost:8080/test"});
+                        }};
     }
 
-    for (int x = 0; x < 10; ++x)
+    for (auto &t : threads)
     {
-        threads[x].join();
+        t.join();
     }
 
-    ASSERT_EQ(0, count);
-    ASSERT_EQ(10, max_count);
+    ASSERT_EQ(thread_count, thread_counter.size());
 }
 
 TEST(server_options, set_connection_limit)
