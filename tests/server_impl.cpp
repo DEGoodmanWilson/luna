e10917ec0bb32045eb5d6b174e218d1a0688f881
Don Goodman-Wilson <don@slack-corp.com>
2017-06-14 17:56:42 +0200

    Tests for the test gods.

--- tests/server_impl.cpp
+++ tests/server_impl.cpp
@@ -8,10 +8,12 @@
 #include <gtest/gtest.h>
 #include <luna/luna.h>
 #include <arpa/inet.h>
+#include <future>
 
 namespace luna
 {
 std::string addr_to_str_(const struct sockaddr *addr);
+request_method method_str_to_enum_(const std::string &method_str);
 bool is_redirect_(status_code code);
 }
 
@@ -40,4 +42,33 @@ TEST(server_impl, is_redirect_)
     ASSERT_TRUE(luna::is_redirect_(301));
     ASSERT_FALSE(luna::is_redirect_(200));
     ASSERT_FALSE(luna::is_redirect_(400));
+}
+
+
+
+TEST(server_impl, method_str_to_enum)
+{
+    ASSERT_EQ(luna::request_method::GET, luna::method_str_to_enum_("GET"));
+    ASSERT_EQ(luna::request_method::GET, luna::method_str_to_enum_("GET"));
+    ASSERT_EQ(luna::request_method::POST, luna::method_str_to_enum_("POST"));
+    ASSERT_EQ(luna::request_method::PUT, luna::method_str_to_enum_("PUT"));
+    ASSERT_EQ(luna::request_method::PATCH, luna::method_str_to_enum_("PATCH"));
+    ASSERT_EQ(luna::request_method::DELETE, luna::method_str_to_enum_("DELETE"));
+    ASSERT_EQ(luna::request_method::OPTIONS, luna::method_str_to_enum_("OPTIONS"));
+}
+
+// check that await actually works, and that we can shut down a server.
+TEST(server_impl, await)
+{
+    luna::server server{luna::server::port{8080}};
+
+    auto future = std::async(
+            std::launch::async, [&server]() {
+                EXPECT_TRUE(static_cast<bool>(server));
+                server.await();
+                EXPECT_FALSE(static_cast<bool>(server));
+            });
+    EXPECT_TRUE(future.wait_for(std::chrono::milliseconds(1000)) == std::future_status::timeout);
+    server.stop();
+    EXPECT_FALSE(future.wait_for(std::chrono::milliseconds(1000)) == std::future_status::timeout);
 }
