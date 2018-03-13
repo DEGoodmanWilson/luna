83ce0d13e4ca4638b1395c3aae90f2f172842f27
Don Goodman-Wilson <don@goodman-wilson.com>
2018-03-13 09:29:17 +0100

    My braindead optional polyfill wasn't getting packaged up. Fixed. Opened an issue to remove it.

--- conanfile.py
+++ conanfile.py
@@ -64,7 +64,8 @@ class LunaConan(ConanFile):
         self.run('cmake --build . %s' % cmake.build_config)
 
     def package(self):
-        self.copy("*.[h|hpp]", dst="include/luna", src="luna")
+        self.copy(pattern="*.h", dst="include/luna", src="luna")
+        self.copy(pattern="*.hpp", dst="include/luna", src="luna")
         self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
         self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
