d557099cc94f67f989f48059c8bffb1f1b0237d6
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-09-24 11:54:27 +0200

    The result of bumping the version. Adds final newslines to a lot of files too. That's OK.

--- conanfile.py
+++ conanfile.py
@@ -19,7 +19,7 @@ import os
 
 class LunaConan(ConanFile):
     name = "luna"
-    version = "5.0.3"
+    version = "5.0.4"
     url = "https://github.com/DEGoodmanWilson/luna.git"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
