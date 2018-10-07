cdc74b6bf9167d4ffba5884ea1f8ae10678ed60f
Don Goodman-Wilson <degoodmanwilson@github.com>
2018-10-07 16:56:19 +0300

    Left off an important python package!

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,6 +21,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 pip install conan --upgrade
-pip install conan_package_tools
+pip install conan_package_tools bincrafters_package_tools
 
 conan user
