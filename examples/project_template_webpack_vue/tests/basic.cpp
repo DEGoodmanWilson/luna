86ce400520ca13ebb5b853bc39e0ab7891377d4f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-18 18:33:37 +0900

    fix tests for webpack_vue

--- examples/project_template_webpack_vue/tests/basic.cpp
+++ examples/project_template_webpack_vue/tests/basic.cpp
@@ -7,12 +7,14 @@ TEST_CASE("basic API functioning")
 {
     auto result = cpr::Get(cpr::Url{hostname + "/api/endpoint"});
     REQUIRE(result.status_code == 200);
-    REQUIRE(result.text == "{\"made_it\": true}");
+    REQUIRE(result.text == "null");
 }
 
 TEST_CASE("basic static asset functioning")
 {
     auto result = cpr::Get(cpr::Url{hostname + "/"});
     REQUIRE(result.status_code == 200);
-    REQUIRE(result.text == "<html>\n<h1>Hello world!</h1>\n</html>");
+    const char* response = "<html>\n<div id=\"app\">\n    <nav-bar></nav-bar>\n    <div class=\"container\">\n        <router-view></router-view>\n    </div>\n</div>\n\n<script src=\"./webpack/index.js\"></script>\n</html>";
+    
+    REQUIRE(result.text == response);
 }
